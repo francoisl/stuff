d8fbfa00fc6a2107fd56dde59c63c1deba27b741
Francois Laithier <francois@expensify.com>
2018-12-28 18:59:23 -0800

    Add compression, make sure dir exists

--- rainbows/functions.sh
+++ rainbows/functions.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-# generateRainbows( sourceFile: string, seedStart: int, filesCount: int )
+# generateFiles( sourceFile: string, seedStart: int, filesCount: int )
 function generateFiles {
     clear
     echo "Generating files, source: $1, seedStart: $2, filesCount: $3"
+    mkdir rnbw 2>/dev/null
     seed=$2
     for index in `seq -f "%04g" 1 $3`;
     do
+        #tput cup 1 0
         filename="rnbw_$index"
         echo "Generating file $filename, seed $seed"
         tput sc
-        tput cup 1 0
-        cat $1 | rnb -S $seed -f > "rnbw/$filename"
+        cat $1 | rnbw -S $seed -f > "rnbw/$filename"
         ((seed++))
     done
     tput rc
+    echo "Combining and compressing files"
+    ( for i in rnbw/rnbw_* ; do cat $i ; echo '___rnbw_file___' ; done ) | gzip -9 > rnbw/combined.gz
     echo Done
 }
